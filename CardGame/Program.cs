cbc2f3e01f3b88612a11f3d90383b0c406fb68af
Martin Raldholm <martin@raldholm.se>
2016-05-29 00:24:57 +0200

    Added classes and enums for cards and decks.

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CardGame
 {
@@ -10,6 +9,40 @@ namespace CardGame
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    class Deck
+    {
+        public enum Suit
+        {
+            Spades, Diamonds, Clubs, Hearts
+        }
+        public enum Value
+        {
+            Ace, Two, Three, Four, Five, Six,
+            Seven, Eight, Nine, Ten, Jack,
+            Queen, King
+        }
+
+        public void CreateBaseDeck()
+        {
+            // Create deck 
+        }
+
+        public void ShuffleDeck()
+        {
+            // Shuffle deck
+        }
+    }
+    class Card
+    {
+        public string suit { get; set; }
+        public string value { get; set; }
+        public Card(string suit, string value)
+        {
+            this.suit = suit;
+            this.value = value;
         }
     }
 }
