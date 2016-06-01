327199cd2a93d4657526ac79e2010325d634996a
Martin Raldholm <martin@raldholm.se>
2016-06-01 22:15:55 +0200

    Todolist

--- CardGame/DeckOfCards.cs
+++ CardGame/DeckOfCards.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+    
+    //todo:
+    /* SKAPA OCH SORTERA KORTLEK METOD:
+     * Sortering skall sortera korten i kortspelet enligt färg och valör: 
+     * Hjärter(Ess,2,3 ... Kung), ruter, klöver och spader.
+     * Alla kortlekar som ingår i spelet skall sorteras.
+     * 
+     * TAKE CARD METOD:
+     * Dra kort skall visa det översta kortet i kortspelet tills dess att alla kort är dragna.
+     * Alla kort ska kunna visas.
+     *  
+     * BLANDA KORTLEK METOD:
+     * Blandning av korten skall lägga korten i kortspelet i slumpvis ordning.             
+    */
 namespace CardGame
 {
     class DeckOfCards : Card
@@ -22,7 +35,7 @@ namespace CardGame
         // Skapa kortlek med 52 kort: 13 värden vardera, med 4 olika kortbeklädnader
 
 
-            // TODO: RETURNERA EN VEKTOR MED EN KORTLEK
+        // TODO: RETURNERA EN VEKTOR MED EN KORTLEK
 
         public void SetUpDeck()
         {
@@ -34,11 +47,11 @@ namespace CardGame
                 // För varje kortbeklädnad, lägg till kortvärde i kortlek
                 foreach (Value v in Enum.GetValues(typeof(Value)))
                 {
-                deck[i] = new Card {MySuit = s, MyValue = v};
+                    deck[i] = new Card { MySuit = s, MyValue = v };
                     i++;
-                }  
+                }
             }
-            
+
         }
     }
 }
